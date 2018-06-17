88b14cd04191d35320e0905c1291f1254aeae13b
Suwitchaya <suwitchayath@gmail.com>
2018-06-17 23:17:48 +0700

    create component addlist and edit app

--- react-todolist/src/App.js
+++ react-todolist/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AddList from "./components/addList.js";
 
 class App extends Component {
   render() {
@@ -8,11 +9,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">TODO List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddList />
+         <div id="myModal" class="modal">
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Some text in the Modal..</p>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ react-todolist/src/components/addList.js
@@ -0,0 +1,12 @@
+import React from "react";
+class AddList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>addList</h1>
+       
+      </div>
+    );
+  }
+}
+export default AddList;
